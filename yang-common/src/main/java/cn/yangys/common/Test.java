baa0959975988813e5483c48ac2306ca9794b06c
youngsun114 <youngsun114@163.com>
2018-05-05 22:23:50 +0800

    make deploy to nexus3 success

---
+++ yang-common/src/main/java/cn/yangys/common/Test.java
@@ -0,0 +1,12 @@
+package cn.yangys.common;
+
+/**
+ * @Author: Yang Yongsheng
+ * @Desciption: This is test class
+ * @Date: Created in 10:08 PM 2018/5/5
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
